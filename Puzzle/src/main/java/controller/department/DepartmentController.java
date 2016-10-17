6c0bad6f7b3e7ed186b0229f186c3b119e8522b0
no <no@nochso-pc>
2016-10-17 14:30:15 +0900

    Depart

--- Puzzle/src/main/java/controller/department/DepartmentController.java
+++ Puzzle/src/main/java/controller/department/DepartmentController.java
@@ -1,42 +1,91 @@
 package controller.department;
 
 import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.List;
-   
+import java.util.Map;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import model.DepartMentCommand;
+import model.MessageCommand;
+import model.PersonnelCommand;
+import model.listCommand;
 import net.sf.json.JSONObject;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/department")
 public class DepartmentController {
 	
 	@Autowired
 	private DepartmentService service;
 	
+	public DepartmentService getService() {
+		return service;
+	}
+
+
 	public void setService(DepartmentService service) {
 		this.service = service;
 	}  
 	
-//	@ModelAttribute("command")
-//	public DepartMentCommand fromBacking(){
-//		return new DepartMentCommand();
-//	}	
+	private int perPage = 6;
 	
-	@RequestMapping(value="/department/departmentList.puzzle", method=RequestMethod.GET)
-	public String dept() throws Exception{
-		return "departmentList";
+	
+	@RequestMapping(value="/departmentList.puzzle", method=RequestMethod.GET)
+	public ModelAndView dept(@RequestParam(value="pageNum", defaultValue = "1")int pageNum) throws Exception{
+		ModelAndView mav =new ModelAndView("departmentList");
+		
+		
+		int totalCount = service.getTotalCount();  //8
+		int pageCount = totalCount/perPage+(totalCount%perPage==0?0:1); //2
+
+		int start = perPage*(pageNum-1)+1; 
+
+		int end = start+(perPage-1) > totalCount ? totalCount : start+(perPage-1);  //6
+		
+		int startPage = (pageNum-1) / 5 * 5 + 1; //1
+		int endPage = startPage + 5 - 1; //5
+		if(endPage>pageCount){
+			endPage=pageCount;
+		}
+		
+		int previous = (pageNum-5)/5*5+1;
+		int next = (pageNum/5+1)*5+1;
+		
+		List<listCommand> list = service.selectAll();
+		mav.addObject("mem", list);
+		
+		System.out.println("totalCount1 : " + totalCount);
+		System.out.println("pageCount1 :"+ pageCount);
+		System.out.println("start1 : " + start);
+		System.out.println("end1 : " + end);
+		System.out.println("pageNum1 : " +pageNum);
+		System.out.println("startPage1 : " +startPage);
+		System.out.println("endPage1 : " + endPage);
+		
+		mav.addObject("startPage1",startPage);
+		mav.addObject("endPage1",endPage);
+		mav.addObject("pageCount1", pageCount);
+		mav.addObject("pageNum1", pageNum);
+		mav.addObject("previous1", previous);
+		mav.addObject("next1", next);
+		
+		return mav;
+
 	}
 	
-	@RequestMapping(value="/department/departmentList.puzzle", method=RequestMethod.POST, produces="application/json; charset=UTF-8")
+	@RequestMapping(value="/departmentList.puzzle", method=RequestMethod.POST, produces="application/json; charset=UTF-8")
 	@ResponseBody
 	public String getList(HttpServletResponse resp, String type, String dept_Num, String dept_Type) throws Exception{
 		resp.setContentType("text/html; charset=UTF-8");
@@ -65,57 +114,56 @@ public class DepartmentController {
 		json.put("list", list);
 		return json.toString();
 	}
-	
-/*	public ModelAndView form(@ModelAttribute("com") DepartMentCommand com) throws Exception{
-		ModelAndView mav = new ModelAndView("department");
-		List<DepartMentCommand> list = service.listdept();
-		
-		mav.addObject("dept_type", list);
-		
-		return mav;
-	}
-		
-	@RequestMapping("/departmentList.puzzle")
+
+	@RequestMapping(value="/departmemList.puzzle", method=RequestMethod.POST, produces="text/plain; charset=UTF-8")
 	@ResponseBody
-	public String insert(HttpServletResponse resp, @ModelAttribute ("com") DepartMentCommand com)throws Exception{
-		resp.setContentType("text/html);charset=utf-8");
-		int a = service.insertDeptType(com);
+	public String getselect(HttpServletResponse resp, String dept_Type) throws Exception{
+		resp.setContentType("text/html; charset=UTF-8");
+
+		listCommand command = new listCommand();
+		command.setDept_Type(dept_Type);
+		int pageNum =1;
+		
+		List<listCommand> dept = service.deptTypeint(command);
+		int totalCount = dept.size();
+		
+		int pageCount = totalCount/perPage+(totalCount%perPage==0?0:1);
+		int start = totalCount-perPage*(pageNum-1); 
+		int end = (start-perPage)+1 > 0 ? (start-perPage)+1 : 1; 
+		
+		int startPage = (pageNum-1) / 5 * 5 + 1; //1
+		int endPage = startPage + 5 - 1; //5
+		if(endPage>pageCount){
+			endPage=pageCount;
+		}
+		
+		int previous = (pageNum-5)/5*5+1;
+		int next = (pageNum/5+1)*5+1;
+		
+		System.out.println("totalCount2 : " + totalCount);
+		System.out.println("pageCount2 :"+ pageCount);
+		System.out.println("start2 : " + start);
+		System.out.println("end2 : " + end);
+		System.out.println("pageNum2 : " +pageNum);
+		
+		List<listCommand> list = service.memList(command);
+		for(int i=0; i<list.size(); i++){
+			list.get(i).setName(URLEncoder.encode(list.get(i).getName(),"UTF-8"));
+			list.get(i).setDept_Type(URLEncoder.encode(list.get(i).getDept_Type(),"UTF-8"));
+//			list.get(i).setAuthority(URLEncoder.encode(list.get(i).getAuthority(),"UTF-8"));
+		}
+		System.out.println(list.size());		
 		JSONObject json = new JSONObject();
-		json.put("dept_type", a);
 		
-		resp.setContentType("text/html);charset=utf-8");
-		PrintWriter out = resp.getWriter();
-		out.print(json.toString());
-		
+
+		json.put("startPage", startPage);
+		json.put("endPage", endPage);
+		json.put("pageCount", pageCount);
+		json.put("pageNum", pageNum);
+		json.put("previous", previous);
+		json.put("next", next);
+		json.put("list", list);
 		return json.toString();
 		
 	}
-
-	
-	@RequestMapping("/update.puzzle")
-	@ResponseBody
-	public String update(HttpServletResponse resp, @ModelAttribute("com") DepartMentCommand com) throws IOException{
-		resp.setContentType("text/html);charset=utf-8");
-		ModelAndView mav = new ModelAndView("department");
-		JSONObject json = new JSONObject();
-		String dept_Num = service.deptnum();
-		mav.addObject("dept_Num", dept_Num);
-		
-		if(dept_Num != null){
-			int a = service.updateDeptType(com);			
-			json.put("dept_type", a);		
-			resp.setContentType("text/html);charset=utf-8");
-			PrintWriter out = resp.getWriter();
-			out.print(json.toString());
-		}
-			return json.toString();
-		
-	}
-	
-	@RequestMapping("/delete.puzzle")
-	public String delete(@RequestParam("num")int dept_Num){
-		service.deleteDeptType(dept_Num);
-		return "redirect:departmentList.puzzle";
-	}*/
-	
 }
--- Puzzle/src/main/java/controller/department/DepartmentService.java
+++ Puzzle/src/main/java/controller/department/DepartmentService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 import mybatis.DepartmentDAO;
 import java.util.List;
 import model.DepartMentCommand;
+import model.PersonnelCommand;
+import model.listCommand;
 
 
 
@@ -30,6 +32,15 @@ public class DepartmentService{
          return list;
    }
 
+   public List<PersonnelCommand> memAll() {
+	      List<PersonnelCommand> list = null;
+	      try{
+	         list = dao.memAll();
+	      }catch(Exception e){
+	    	  System.out.println(e.toString());
+	      }
+	         return list;
+	   }
    
    public void insertDeptType(DepartMentCommand command){
       try{
@@ -57,6 +68,89 @@ public class DepartmentService{
          System.out.println(e.toString());
       }
    }
+   
+   public List<listCommand> selectAll(){
+	   List<listCommand> list = null;
+	   try{
+		   list = dao.listAll();
+/*		   System.out.println("service "+list.size());
+		   for(listCommand lis:list){
+				System.out.println("ser"+lis.getName());
+		   }*/
+	   }catch (Exception e) {
+		  System.out.println(e.toString());
+	   }
+	   
+	   return list;
+   }
+   
+   public List<PersonnelCommand> workAll(){
+	   List<PersonnelCommand> list = null;
+	   try{
+		   list = dao.workAll();
+	   }catch (Exception e) {
+		  System.out.println(e.toString());
+	   }
+	   
+	   return list;
+   }
+   
+   public List<PersonnelCommand> timeoffAll(){
+	   List<PersonnelCommand> list = null;
+	   try{
+		   list = dao.timeoffAll();
+	   }catch (Exception e) {
+		  System.out.println(e.toString());
+	   }
+	   
+	   return list;
+   }
+   
+   public List<PersonnelCommand> resignAll(){
+	   List<PersonnelCommand> list = null;
+	   try{
+		   list = dao.resignAll();
+	   }catch (Exception e) {
+		  System.out.println(e.toString());
+	   }
+	   
+	   return list;
+   }
+   
+   public List<listCommand> memList(listCommand command){
+	   List<listCommand> list = null;
+	   try{
+		   list = dao.detpTypeList(command);
+
+	   }catch(Exception e){
+		   System.out.println(e.toString());
+	   }
+	   return list;
+   }
+   public List<listCommand> deptTypeint(listCommand command){
+	   List<listCommand> list = null;
+	   try{
+		   list = dao.deptTypeint(command);
+	   }catch(Exception e){
+		   System.out.println(e.toString());
+	   }
+	   
+	   return list;
+	   
+   }
+   public int getTotalCount(){
+	   System.out.println("SERVICE Á¢±Ù");
+		int count = 0;
+		try {
+			count = dao.getTotalCount();
+			System.out.println("count : " + count);
+		} catch(Exception e){
+			System.out.println(e.toString());
+		}
+		return count;
+	}
+	
+   
 }
 
 
