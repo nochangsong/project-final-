96d8138db549142a1b1d5a4fe3a4dc7a640b6e0f
Yeonsil <Yeonsil@YEONSILKOO>
2016-10-17 13:28:24 +0900

    1:28

--- Puzzle/src/main/java/controller/main/MainController.java
+++ Puzzle/src/main/java/controller/main/MainController.java
@@ -1,46 +1,14 @@
 package controller.main;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import model.DepartMentCommand;
 
 @Controller
 @RequestMapping("/main")
 public class MainController {
 
-//	@Autowired
-//	private DepartMentCommand com;
-//	public void setCom(DepartMentCommand com) {
-//		this.com = com;
-//	}
-//	
-
-	@ModelAttribute("com")
-	public DepartMentCommand formBacking(){
-	     return new DepartMentCommand();
-	}
-	   
-	public MainController() {
-			// TODO Auto-generated constructor stub
-		}
-
 	@RequestMapping("main.puzzle")
 	public String main(){
 		return "main";
-	}
-	
-
-	@RequestMapping("/admin.puzzle")
-	public String adminlogin(){
-		
-		return "admin";
-	}
-	
-	@RequestMapping("/user.puzzle")
-	public String userlogin(){
-		return "user";
 	}
 }
